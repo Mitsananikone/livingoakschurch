eaa549472ccf647e08fce4113021a614cb26ecbe
Mit Sananikone <Mitsananikone@gmail.com>
2025-06-13 13:42:35 -0500

    Homepage title font 2rem to 1.8rem

--- pages/home/homepage.jsx
+++ pages/home/homepage.jsx
@@ -32,7 +32,7 @@ function HomePage() {
       opacity: fadeInImages ? 1 : 0,
     },
     title: {
-    fontSize: screenWidth < 480 ? "2rem" :  // Extra small devices
+    fontSize: screenWidth < 480 ? "1.8rem" :  // Extra small devices
              screenWidth < 768 ? "1.7rem" :     // Small devices
              screenWidth < 1024 ? "1.6rem" : // Medium devices
              "2rem",                       // Large devices (1024px and up)
